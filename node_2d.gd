51c3278723de5b115256f425a3d09a78c7976fa5
Jantunen Paula <paula.jantunen@edu.espoo.fi>
2025-12-23 23:57:48 +0200

    kaks

--- node_2d.gd
+++ node_2d.gd
@@ -4,23 +4,27 @@ var leftovers := 0
 
 @onready var label = $LeftoversLabel
 @onready var sound = $SoundPlayer
+@onready var rat = $Rat
 
 func _ready():
 	update_label()
 
-func _on_Fridge_pressed():
-	leftovers += 1
-	update_label()
-	if leftovers == 10:
-		sound.stream = preload("res://sounds/xylophone2.wav")
-	
-	sound.play()
-
 func update_label():
 	label.text = "Lefovers: %d" % leftovers
+	if leftovers < 10:
+		rat.modulate = Color.WHITE
+	elif leftovers < 25:
+		rat.modulate = Color(1, 1, 0.8) # warm
+	else:
+		rat.modulate = Color(1, 0.8, 0.8) # slightly cursed
 
 func _on_fridge_pressed() -> void:
 	leftovers += 1
 	update_label()
 	if leftovers == 10:
 		sound.stream = preload("res://sounds/xylophone2.wav")
+	sound.play()
+	rat.scale = Vector2(1.1, 0.9) # squish
+
+func _process(delta):
+	rat.scale = rat.scale.lerp(Vector2.ONE, 8 * delta)
